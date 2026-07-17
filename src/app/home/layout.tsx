59381530f95c2b8480a9364abd794bfe24110199
Ritik <youremail@gmail.com>
2026-07-17 22:00:56 +0530

    Fix layout syntax duplication to fully activate Aurelia OS home route on vercel edge network

--- src/app/home/layout.tsx
+++ src/app/home/layout.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 
-// 👑 यह जादुई मास्टर कमान पूरे /home फ़ोल्डर के सभी एआई और एनिमेटेड लूप्स को सीधे लाइव डायनेमिक मोड पर लॉक कर देगी!
-export const dynamic = 'force-dynamic';
-
+// 👑 Aurelia OS का शुद्ध पैरेंट लेआउट, बिना किसी डुप्लिकेट कॉन्फ़िगरेशन टकराव के
 export default function HomeFolderLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
